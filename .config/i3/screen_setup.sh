43aff56f2343d056b0cf8f2dea1572ed54c38e58
Ruben Gysemans <ruben@mobiqam.com>
2024-02-22 09:49:27 +0100

    Sync with work pc

---
+++ .config/i3/screen_setup.sh
@@ -0,0 +1,7 @@
+## Work settings
+#xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --dpi 200 --output DP-0 --mode 3840x2160 --pos 3840x0 --rotate normal --dpi 200 --output DP-1 --off
+#xset mouse 8 4
+
+## Home settings
+#xrandr --output DVI-D-1 --off --output DP-1 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --mode 1920x1080 --pos 0x1080 --rotate normal
+#xset mouse 8 4
